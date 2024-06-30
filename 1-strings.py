0de6dcb10f0712edde50966ee8335b8eebeb8ce2
chimaobi <okehchimaobi@gmail.com>
2024-06-30 11:49:09 +0100

    updated the first 3 chapters on the notebook

--- 1-strings.py
+++ 1-strings.py
@@ -1,39 +1,39 @@
-#09-05-2024
+# 09-05-2024
 
-#print('hello world')
-
-
-#input and F-strings   
-# name = input('Enter your name: ')
-# age = input('Enter your age: ')
-# address = input('Enter your address: ')
-# print(f'Good Morning Mr {name} your are {age} years old and you live at no {address}')
+print('hello world')
 
 
- #indexing (slicing)
-# x = "Do you know that I love python"
-# print(x[5])
+# input and F-strings   
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+address = input('Enter your address: ')
+print(f'Good Morning Mr {name} your are {age} years old and you live at no {address}')
 
-#UPPER
-# print(x[3:6].upper())
+
+#  indexing (slicing)
+x = "Do you know that I love python"
+print(x[5])
+
+# UPPER
+print(x[3:6].upper())
 
 # #combining them all
-# y = x[0:3] + x[3:6].upper() + x[6:]
-# print(y)
+y = x[0:3] + x[3:6].upper() + x[6:]
+print(y)
 
-#Assignment
+# Assignment
 # A1 = print('write a statement using INDEXING, CONCATNATION & .UPPER function')
 # print(A1)
 
 
-#LOWER function
+# LOWER function
 # .lower()
 
-#CAPITALIZE function
+# CAPITALIZE function
 # .capitalize() this capitalizes the first letter
 
 
-#STRIP function
+# STRIP function
 # .strip() ' this is used to clear white spaces 
 # "e.g mistakenly pressing space bar after putting password on investment site"
 
@@ -43,32 +43,26 @@
 
 
 # INDEX function
-#this is used to find the index of the letter that preceeds "welcome"
-# x = 'Hello, welcome to my world'
-# y = x.index('welcome')
-# print(y)
+# this is used to find the index of the letter that preceeds "welcome"
+x = 'Hello, welcome to my world'
+y = x.index('welcome')
+print(y)
 
 
 # Escape characters \
 # used to insert an illegal character within a string
-# x = "Jesus said \"Blessed are the pure in spirit\" thank you"
+x = "Jesus said \"Blessed are the pure in spirit\" thank you"
 # #alternatively
-# # x = 'Jesus said "Blessed are the pure in spirit" thank you'
-# # print(x)
-# #another escape character is \n
-# #this is used to enter a new line
-# # x = "Jesus said \"Blessed are the pure in spirit\"\n thank you"
-# # print(x)
+x = 'Jesus said "Blessed are the pure in spirit" thank you'
+print(x)
+# another escape character is \n
+# this is used to enter a new line
+x = "Jesus said \"Blessed are the pure in spirit\"\n thank you"
+print(x)
 # #another on is \t
 # #this is used to put a tab withn a string
 
 # # #ASSIGNMENT
-# # temp = int(input('what is the temperature^C: '))
-# # conv = temp * (9/5) + 32
-
-# # result = 'the temperature in ^F is '
-
-# # print(str(temp) +' is the temperature in ^C but in ^F it is ' + str(conv)) 
-
-# # print(result + conv)
-# # print({conv} + ' is the temperature in')
+temp = int(input('what is the temperature^C: '))
+conv = temp * (9/5) + 32
+print(str(temp) +' is the temperature in ^C but in ^F it is ' + str(conv)) 
--- 2-operators.py
+++ 2-operators.py
@@ -3,57 +3,58 @@
 #ARITHEMETIC OPERATORS
 
 
-# # x = 7
-# # y = '2'
-# # result = x + y
-# # print(result) 
+x = 7
+y = '2' #Error because you cant concatenate (add +) a string with an integer
+result = x + y
+print(result) 
 
 
 # #USE OF MODULUS
-# x = int(input('Enter the number:'))
-# if x % 2 == 0:
-#     print(f'the number {x} is an even number')
-#     print(x,'the number is an even number')
-#     print('the number ' + str(x) + ' is an even number')
-# else:
-#     print(f'the number {x} is an odd number')
-#     print(x,'the number is an odd number')
-#     print('the number ' + str(x) + ' is an odd number')
+x = int(input('Enter the number:'))
+if x % 2 == 0:
+    print(f'the number {x} is an even number')
+    print(x,'the number is an even number')
+    print('the number ' + str(x) + ' is an even number')
+else:
+    print(f'the number {x} is an odd number')
+    print(x,'the number is an odd number')
+    print('the number ' + str(x) + ' is an odd number')
 
 
-# #FLOAT DIVISION / INTEGER DIVISION
-# x = 7
-# y = 2
-# result = x // y
-# print(result)
+#FLOAT DIVISION / INTEGER DIVISION
+x = 7
+y = 2
+result = x // y
+print(result)
 
 
 #ASSIGNMENT OPERATORS
-# x = 5
-# y = 2
-# x = y
-# print(x)
+x = 5
+y = 2
+x = y
+print(x)
 
-# x = 7
+x = 7
 
-# x += 3
-# x = x + 3
+x += 3
+x = x + 3
 
-# x -= 3
-# x = x - 3
+x -= 3
+x = x - 3
 
-# x *= 3
-# x = x * 3
+x *= 3
+x = x * 3
 
-# print(x)
+print(x)
 
 #LOGIC OPERATORS
-# x = 5
-# print(x > 5 and x < 10)
+x = 5
+print(x > 5 and x < 10)
 
-
-# x = 'hello world'
-# print('hello' not in x)
-# y = 'hello world'
-# print(x is y)
-# print(x is not y)
+#MEMBERSHIP & iDENTITY OPERATORS
+x = 'hello world'
+print('hello' not in x)
+y = 'hello world'
+print(x in y)
+print(x is y)
+print(x is not y)
